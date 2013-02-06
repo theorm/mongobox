afb95fdc7ca25fee535f2d3951e1dc651d50665c
theorm <theorm@gmail.com>
2013-02-07 10:34:04 +1100

    - added manifest, updated setup.py and fixed version

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
 from setuptools import setup, find_packages
 
-VERSION='0.1.1'
+VERSION='0.1.0'
 
 setup(name='mongobox',
       version=VERSION,
+      description="Run sandboxed Mongo DB instance from a python application.",
+      long_description=open("README.md").read(),
+      url='http://github.com/theorm/mongobox',
+      license=open("LICENSE").read(),
+      author='Roman Kalyakin',
+      author_email='roman@kalyakin.com',
+      packages=find_packages(exclude=('tests',)),
+      package_data={'':['LICENSE','*.md']},
+      include_package_data=True,
+      install_requires=[],
+      zip_safe=False,
       classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Plugins',
@@ -14,19 +25,8 @@ setup(name='mongobox',
         'Topic :: Database :: Database Engines/Servers',
         'Topic :: Software Development :: Testing'
       ],
-      author='Roman Kalyakin',
-      author_email='roman@kalyakin.com',
-      description="Run sandboxed Mongo DB instance from a python application.",
-      long_description=open("README.md").read(),
-      url='http://github.com/theorm/mongobox',
-      license=open("LICENSE").read(),
-			package_data={'':['LICENSE','README.md','AUTHORS.md']},
-      packages=find_packages(exclude=('tests',)),
-      install_requires=[],
-      include_package_data=True,
       tests_require=["nose>=0.10","pymongo>=2.0"],
       test_suite = "nose.collector",
-      zip_safe=True,
       entry_points={
         'nose.plugins.0.10': [
             'mongobox = mongobox.nose_plugin:MongoBoxPlugin',
